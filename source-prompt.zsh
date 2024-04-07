72729aaf325d10006c6609761e1011e113063824
Christian Hoock <9932295+wk1@users.noreply.github.com>
2024-04-07 22:55:52 +0200

    Update source-prompt.zsh

--- source-prompt.zsh
+++ source-prompt.zsh
@@ -5,7 +5,8 @@ prompt_path="$(brew --prefix)/share/zsh_prompt"
 
 # Überprüfung, ob der source-Befehl bereits in .zshrc existiert
 if ! grep -qF "source $prompt_path" ~/.zshrc; then
-  echo "source $prompt_path" >> ~/.zshrc
+  # Fügt den source-Befehl am Anfang von .zshrc hinzu
+  echo "source $prompt_path\n$(cat ~/.zshrc)" > ~/.zshrc
   echo "zsh_prompt wurde zu .zshrc hinzugefügt."
 else
   echo "zsh_prompt ist bereits in .zshrc vorhanden."
